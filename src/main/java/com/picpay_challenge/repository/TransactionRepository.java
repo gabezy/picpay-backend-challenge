e21a3debc9e544651bcc6d83e395f69f45ccdc17
gabezy <gabrielmoreira2705@gmail.com>
2023-08-16 13:17:02 -0300

    feat: update Transaction package name

--- src/main/java/com/picpay_challenge/repository/TransactionRepository.java
+++ src/main/java/com/picpay_challenge/repository/TransactionRepository.java
@@ -1,6 +1,6 @@
 package com.picpay_challenge.repository;
 
-import com.picpay_challenge.domain.Transaction.Transaction;
+import com.picpay_challenge.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
