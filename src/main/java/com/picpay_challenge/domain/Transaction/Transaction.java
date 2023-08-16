8520da4dc8de7aa96f2d182c17a44c4031245f31
gabezy <gabrielmoreira2705@gmail.com>
2023-08-15 23:29:07 -0300

    feat: create transaction Entity

---
+++ src/main/java/com/picpay_challenge/domain/Transaction/Transaction.java
@@ -0,0 +1,31 @@
+package com.picpay_challenge.domain.Transaction;
+
+import com.picpay_challenge.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal value;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+
+}
